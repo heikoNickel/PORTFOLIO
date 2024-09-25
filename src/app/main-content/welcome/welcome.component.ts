605409b5d695cb21d3919710dc14c1ae397e1562
Heiko Nickel <heiko.nickel@gmx.net>
2024-09-26 00:18:54 +0200

    Second Commit

--- src/app/main-content/welcome/welcome.component.ts
+++ src/app/main-content/welcome/welcome.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
-})
+})  
 export class WelcomeComponent {
 
 }
