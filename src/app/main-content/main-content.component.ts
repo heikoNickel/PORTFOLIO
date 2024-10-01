97154ce948fb5c5ad9a3743bb2ba382c19b8b63c
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-01 13:53:02 +0200

    skills section nearly ready
    background green missing

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [WelcomeComponent, AboutMeComponent],
+  imports: [WelcomeComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface ICON{
+  icon: string;
+  description: string;
+}
+
+
 @Component({
   selector: 'app-skills',
   standalone: true,
@@ -8,5 +14,56 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  icons: ICON []=[
+    {
+      icon: "./assets/img/my-skills/html.png",
+      description: "HTML"
+    },
+    {
+      icon: "./assets/img/my-skills/css.png",
+      description: "CSS"
+    },
+    {
+      icon: "./assets/img/my-skills/java-script.png",
+      description: "JavaScript"
+    },
+    {
+      icon: "./assets/img/my-skills/type-script.png",
+      description: "TypeScript"
+    },
+    {
+      icon: "./assets/img/my-skills/angular.png",
+      description: "Angular"
+    },
+    {
+      icon: "./assets/img/my-skills/firebase.png",
+      description: "Firebase"
+    },
+    {
+      icon: "./assets/img/my-skills/git.png",
+      description: "GIT"
+    },
+    {
+      icon: "./assets/img/my-skills/rest-api.png",
+      description: "Rest-API"
+    },
+    {
+      icon: "./assets/img/my-skills/scrum.png",
+      description: "Scrum"
+    },
+    {
+      icon: "./assets/img/my-skills/material-design.png",
+      description: "Material Design"
+    },
+    {
+      icon: "./assets/img/my-skills/continually-learning.png",
+      description: "Continually Leraning"
+    },
+  ];
 
+
+
+
+
+  
 }
