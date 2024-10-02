c0f91a3c51b441ce3dff224fa2469175c09a76a3
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-03 01:17:26 +0200

    Portfolio ready
    responsive included

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 interface PROJECT{
   name: string;
   img: string;
-  skills: string[];
+  skills: string;
   description: string;
   liveTestLink: string;
   gitHubLink: string;
@@ -17,5 +17,30 @@ interface PROJECT{
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  project: PROJECT []=[
+    {
+      name: "Join",
+      img: "./assets/img/portfolio/join.png",
+      skills: "Angular | TypeScript | HTML | CSS | Firebase",
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      liveTestLink: "blabla",
+      gitHubLink: "blubblub",
+    },
+    {
+      name: "El Pollo Loco",
+      img: "./assets/img/portfolio/pollo-loco.png",
+      skills: "JavaScript | HTML | CSS",
+      description: "A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the chicken endboss..",
+      liveTestLink: "blabla",
+      gitHubLink: "blubblub",
+    },
+    {
+      name: "Pokedex",
+      img: "./assets/img/portfolio/pokedex.png",
+      skills: "JavaScript | HTML | CSS | API",
+      description: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.",
+      liveTestLink: "blabla",
+      gitHubLink: "blubblub",
+    },
+  ];
 }
