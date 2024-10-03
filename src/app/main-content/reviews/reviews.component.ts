a50e9ee3b04ef6133508d0093362478fde18d426
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-04 00:22:53 +0200

    Reviews Third edit
    incl. responsive and rendern

--- src/app/main-content/reviews/reviews.component.ts
+++ src/app/main-content/reviews/reviews.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface REVIEWS{
+  name: string;
+  title: string;
+  review: string;
+}
+
 @Component({
   selector: 'app-reviews',
   standalone: true,
@@ -8,5 +14,45 @@ import { Component } from '@angular/core';
   styleUrl: './reviews.component.scss'
 })
 export class ReviewsComponent {
+  review: REVIEWS []=[
+    {
+      name: "V.Schuster",
+      title: "Team Partner",
+      review: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+    },
+    {
+      name: "E.Eichinger",
+      title: "Team Partner",
+      review: "Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.",
+    },
+    {
+      name: "I.Nuber",
+      title: "Frontend Engineer",
+      review: "MIt was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well..",
+    },
+  ];
+
+  currentIndex: number = 0;
+
+  increaseIndex(){
+    if(this.currentIndex < this.review.length - 1){
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0
+    } 
+  }
+
+  decreaseIndex(){
+    if(this.currentIndex > 0){
+      this.currentIndex --;
+    } else {
+      this.currentIndex = this.review.length - 1;
+    } 
+  }
+
+  get currentReview() {
+    return this.review[this.currentIndex];
+  }
+
 
 }
