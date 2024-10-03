468fe6926b15f807112aa0ea85a2f10223839fa9
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-03 13:27:25 +0200

    reviews first edit

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from "./skills/skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [WelcomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
+  imports: [WelcomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReviewsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
