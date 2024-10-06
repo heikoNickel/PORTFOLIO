85812aa8f4458b50bd225b31c0af78dd6b5b3c88
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-07 00:36:06 +0200

    contact form
    first edit

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from "./skills/skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [WelcomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReviewsComponent],
+  imports: [WelcomeComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReviewsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
