d76fdd3271a921c8c22e1a84aa4b57f4f46f2c48
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-06 21:58:53 +0200

    Reviews finished
    background and apostroph added

--- src/app/main-content/reviews/reviews.component.ts
+++ src/app/main-content/reviews/reviews.component.ts
@@ -4,6 +4,7 @@ interface REVIEWS{
   name: string;
   title: string;
   review: string;
+  img: string;
 }
 
 @Component({
@@ -18,16 +19,19 @@ export class ReviewsComponent {
     {
       name: "V.Schuster",
       title: "Team Partner",
+      img: "./assets/img/reviews/man.png",
       review: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
     },
     {
       name: "E.Eichinger",
       title: "Team Partner",
+      img: "./assets/img/reviews/user.png",
       review: "Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.",
     },
     {
       name: "I.Nuber",
       title: "Frontend Engineer",
+      img: "./assets/img/reviews/woman.png",
       review: "MIt was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well..",
     },
   ];
