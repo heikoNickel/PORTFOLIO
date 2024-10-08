6f155592929d28092883bd3b7db3e6eea6c4ee0b
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-08 23:27:54 +0200

    Form validation contact
    Form validation works

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, CommonModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('^[A-Za-z ]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(4)]],
+      privacy: [false, [Validators.requiredTrue]]
+    });
+  }
+
+  onFocus(controlName: string) {
+    this.contactForm.controls[controlName].markAsTouched();
+  }
+
+  onCheckboxChange() {
+    this.contactForm.controls['privacy'].markAsTouched();
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted', this.contactForm.value);
+
+      this.contactForm.reset({
+        name: '',
+        email: '',
+        message: '',
+        privacy: false
+      });
+    }
+  }
 }
