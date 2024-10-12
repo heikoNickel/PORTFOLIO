6cfff5df1e3f2bf8b661f7d51112d512785c1354
Heiko Nickel <heiko.nickel@gmx.net>
2024-10-12 23:41:41 +0200

    contact translate
    translate buttons styling,...

--- src/app/translation.service.ts
+++ src/app/translation.service.ts
@@ -6,13 +6,24 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class TranslationService {
 
+  public currentLanguage: string = 'en';
+
   constructor(private translate: TranslateService) {
     // Standard-Sprache setzen
-    this.translate.setDefaultLang('en');
+    this.translate.setDefaultLang(this.currentLanguage);
   }
 
-  public switchLanguage(language: string): void{
+  // public switchLanguage(language: string): void{
+  //   this.translate.use(language);
+  // }
+
+  public switchLanguage(language: string): void {
+    this.currentLanguage = language; // Setze die aktuelle Sprache
     this.translate.use(language);
+  }
+
+  public getCurrentLanguage(): string {
+    return this.currentLanguage; // Methode, um die aktuelle Sprache zu erhalten
   }
 }
 
